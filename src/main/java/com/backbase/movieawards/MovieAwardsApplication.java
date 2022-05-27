19b208823b41988080629555a7bb173b52ebd110
tchristodoulou <84441101+tchristodoulou@users.noreply.github.com>
2022-05-27 09:19:59 +0100

    SETUP: Initial creation of project

---
+++ src/main/java/com/backbase/movieawards/MovieAwardsApplication.java
@@ -0,0 +1,13 @@
+package com.backbase.movieawards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieAwardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieAwardsApplication.class, args);
+	}
+
+}
